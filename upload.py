8333870b39c42b657d27549458c7f6aab7eec8bb
raspi <julian_ullrich@yahoo.com>
2025-05-07 04:51:52 +0200

    final touches

--- upload.py
+++ upload.py
@@ -2,6 +2,7 @@ import dropbox
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+import csv
 
 load_dotenv()
 
@@ -10,13 +11,19 @@ drpbx = dropbox.Dropbox(API_KEY)
 
 data_folder = Path(__file__).resolve().parent / "_data"
 
+current_csv = data_folder / "api_data_current.csv"
+with open(current_csv, newline = "", encoding="utf-8") as f:
+    reader = csv.reader(f)
+    next(reader)
+    first_row = next(reader)
+    date_str = first_row[0].split()[0]
+
+dropbox_folder = f"/{date_str}"
+
 for local_file in data_folder.glob("*.csv"):
-    dropbox_file = f"/{local_file.name}"
+    dropbox_file = f"{dropbox_folder}/{local_file.name}"
     with open(local_file, "rb") as file:
         drpbx.files_upload(file.read(), dropbox_file, mode=dropbox.files.WriteMode("overwrite"))
-    print(f"uploaded {local_file.name}")
 
 for csv_file in data_folder.glob("*.csv"):
     csv_file.write_text("")
-
-print("cleared")
