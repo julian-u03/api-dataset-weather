eb22831a170fc789f55c803399a46a35a2c03e09
raspi <julian_ullrich@yahoo.com>
2025-05-08 04:33:15 +0200

    change upload

--- upload.py
+++ upload.py
@@ -3,15 +3,24 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 import csv
+from dropbox.oauth import DropboxOAuth2FlowNoRedirect
+from dropbox.dropbox_client import Dropbox
 
 load_dotenv()
 
-API_KEY = os.getenv("API_KEY_dropbox")
-drpbx = dropbox.Dropbox(API_KEY)
+APP_KEY = os.getenv("APP_KEY_dropbox")
+APP_SECRET = os.getenv("APP_SECRET_dropbox")
+REFRESH_TOKEN = os.getenv("REFRESH_TOKEN_dropbox")
+
+drpbx = Dropbox(
+    oauth2_refresh_token=REFRESH_TOKEN,
+    app_key=APP_KEY,
+    app_secret=APP_SECRET
+)
 
 data_folder = Path(__file__).resolve().parent / "_data"
-
 current_csv = data_folder / "api_data_current.csv"
+
 with open(current_csv, newline = "", encoding="utf-8") as f:
     reader = csv.reader(f)
     next(reader)
